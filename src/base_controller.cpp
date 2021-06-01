6bba0c5c7ace15c1bdf33ae76e0747a458892aed
mariaedelgado <maria.delgado.tejedor@gmail.com>
2021-06-01 09:15:01 -0700

    feat> odom left right

--- src/base_controller.cpp
+++ src/base_controller.cpp
@@ -12,16 +12,15 @@
 
 /* Variable definitions */
 
-double vel_actual_right = 0.0;
-double vel_actual_left = 0.0;
-double vel_dt = 0.0;
-double vel_translational = 0.0;
-double vel_angular = 0.0;
+double vel_actual_right;
+double vel_actual_left;
+double vel_translational;
+double vel_angular;
 
 double dx = 0.0;
 double dy = 0.0;
 double dtheta = 0.0;
-double dt = 0.0;
+double dt;
 
 double x_pos = 0.0;
 double y_pos = 0.0;
@@ -40,8 +39,8 @@ void vel_callback(const geometry_msgs::Vector3Stamped& msg)
 {
   vel_actual_right = msg.vector.x;
   vel_actual_left = msg.vector.y;
-  vel_dt = msg.vector.z;
-  ROS_INFO("**Subscribed to /vel** Velocity right: %f, Velocity left: %f, Velocity dt: %f", vel_actual_right, vel_actual_left, vel_dt);
+  dt = msg.vector.z;
+  //ROS_INFO("**Subscribed to /vel** Velocity right: %f, Velocity left: %f, Velocity dt: %f", vel_actual_right, vel_actual_left, vel_dt);
 }
 
 /* Main function*/
@@ -58,24 +57,44 @@ int main(int argc, char **argv)
   ros::Rate r(10);
 
   ROS_INFO("Start reading incoming messages ...");
+
   while(ros::ok()) {
     ros::spinOnce();
     current_time = ros::Time::now();
  
     /* Transform from velocity to position */
     vel_translational = (vel_actual_right + vel_actual_left)/2;
-    vel_angular = (vel_actual_right - vel_actual_left)/track_width;
-    
-    dtheta = vel_angular;
-    dx = cos(theta)*vel_translational;
-    dy = sin(theta)*vel_translational;
+    vel_angular = ((vel_actual_right - vel_actual_left)*(wheel_diameter/2))/track_width;
+    ROS_INFO("VEL TRANS %f, VEL ANGULAR %f", vel_translational, vel_angular);
 
-    x_pos += (dx*cos(dtheta) - dy*sin(dtheta)) *dt;
-    y_pos += (dx*sin(dtheta) + dy*cos(dtheta)) *dt;
+    dtheta = vel_angular;
+    dx = cos(dtheta)*vel_translational;
+    dy = -sin(dtheta)*vel_translational;
+    
+
+    x_pos += (dx*cos(theta) - dy*sin(theta)) *dt;
+    y_pos += (dx*sin(theta) + dy*cos(theta)) *dt;
     theta += dtheta*dt;
 
-    /* Publish Odometry topic */
+    //if(theta >= two_pi) theta -= two_pi;
+    //if(theta <= -two_pi) theta += two_pi;
+
+    ROS_INFO("**X POS %f, Y POS %f, THETA %f", x_pos, y_pos, theta);
+
+    /* Publish TF */
     geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(theta);
+    geometry_msgs::TransformStamped t;
+    t.header.frame_id = odom;
+    t.child_frame_id = base_link;
+    t.transform.translation.x = x_pos;
+    t.transform.translation.y = y_pos;
+    t.transform.translation.z = 0.0;
+    t.transform.rotation = odom_quat;
+    t.header.stamp = current_time;
+
+    broadcaster.sendTransform(t);
+
+    /* Publish Odometry topic */
     nav_msgs::Odometry odom_msg;
     odom_msg.header.stamp = current_time;
     odom_msg.header.frame_id = odom;
@@ -121,9 +140,10 @@ int main(int argc, char **argv)
     odom_msg.twist.twist.linear.x = dx;
     odom_msg.twist.twist.linear.y = 0.0;
     odom_msg.twist.twist.angular.z = dtheta;
+    
     odometry_publisher.publish(odom_msg);
 	
-    ROS_INFO("**Publishing /odom** X: %f, Y: %f, Theta: %f", x_pos, y_pos, theta);
+    //ROS_INFO("**Publishing /odom** X: %f, Y: %f, Theta: %f", x_pos, y_pos, theta);
     last_time = current_time;
     r.sleep();
   }
